4ddd4e37575779dbbeab028cffb53fd39b949b49
aljithkj02 <kwdeathlok@gmail.com>
2024-06-19 15:52:21 +0530

    add dummy Rooms side bar

---
+++ client/src/components/home/Rooms.tsx
@@ -0,0 +1,20 @@
+
+export const Rooms = () => {
+  return (
+    <div className="border-r-2 max-h-[91.5vh] h-screen overflow-y-auto">
+      <p className="text-violet-700 font-semibold text-xl text-center mt-2">Available Rooms</p>
+
+      <div className="flex flex-col gap-2 mt-6">
+        <p className="px-5 cursor-pointer hover:bg-yellow-200 py-2 text-lg transition-all duration-300">Room1</p>
+        <p className="px-5 cursor-pointer hover:bg-yellow-200 py-2 text-lg transition-all duration-300">Room1</p>
+        <p className="px-5 cursor-pointer hover:bg-yellow-200 py-2 text-lg transition-all duration-300">Room1</p>
+        <p className="px-5 cursor-pointer hover:bg-yellow-200 py-2 text-lg transition-all duration-300">Room1</p>
+        <p className="px-5 cursor-pointer hover:bg-yellow-200 py-2 text-lg transition-all duration-300">Room1</p>
+        <p className="px-5 cursor-pointer hover:bg-yellow-200 py-2 text-lg transition-all duration-300">Room1</p>
+        <p className="px-5 cursor-pointer hover:bg-yellow-200 py-2 text-lg transition-all duration-300">Room1</p>
+        <p className="px-5 cursor-pointer hover:bg-yellow-200 py-2 text-lg transition-all duration-300">Room1</p>
+        <p className="px-5 cursor-pointer hover:bg-yellow-200 py-2 text-lg transition-all duration-300">Room1</p>
+      </div>
+    </div>
+  )
+}
