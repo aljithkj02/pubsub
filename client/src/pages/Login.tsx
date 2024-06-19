947eacf7981cfaaa8bf9e7319ab0090d2547173a
aljithkj02 <kwdeathlok@gmail.com>
2024-06-19 20:21:59 +0530

    add dispatch action to turn on and off the loader

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,13 +1,18 @@
 import { AuthPage, UserInfo } from "@src/components/auth/AuthPage"
 import { loginUser } from "@src/services/auth";
+import { setLoading } from "@src/store/slices/global.slice";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const login = async ({name, email, password}: UserInfo) => {
+    dispatch(setLoading(true))
     const res = await loginUser({ name, email, password });
+    dispatch(setLoading(false))
 
     if (res) {
       navigate('/');
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,13 +1,18 @@
 import { AuthPage, UserInfo } from "@components/auth/AuthPage"
 import { signupUser } from "@src/services/auth";
+import { setLoading } from "@src/store/slices/global.slice";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 export const Signup = () => {
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const signup = async ({name, email, password}: UserInfo) => {
+    dispatch(setLoading(true));
     const res = await signupUser({ name, email, password });
+    dispatch(setLoading(false));
 
     if (res) {
       navigate('/');
