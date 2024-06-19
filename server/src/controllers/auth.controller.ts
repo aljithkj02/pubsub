949596813ccd983034c1036ff5dd58067c45dcf6
aljithkj02 <kwdeathlok@gmail.com>
2024-06-19 10:50:19 +0530

    add signup handler function

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { validateSignupPayload } from "@lib/validators/auth.validator";
 import { handleValidationError } from "@lib/exceptions";
+import prisma from "@lib/db";
+import bcrypt from 'bcryptjs'
+import { generateToken } from "@lib/utils/jwt.util";
 
 export const signupUser = async (req: Request, res: Response) => {
     try {
@@ -9,6 +12,35 @@ export const signupUser = async (req: Request, res: Response) => {
         if (!validateResult.success) {
             return handleValidationError(res, validateResult.error);
         }
+
+        const {name, email, password } = validateResult.data;
+
+        const isUserExist = await prisma.user.findUnique({ where: { email }});
+
+        if (isUserExist) {
+            return res.json({
+                status: false,
+                message: 'A user is already exist with this email!'
+            })
+        }
+
+        const hashedPassword = bcrypt.hashSync(password, 10);
+
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword
+            }
+        })
+
+        const token = generateToken({ id: user.id, name: user.name });
+
+        return res.json({
+            status: true,
+            message: 'Signup Succeed!',
+            token
+        })
     } catch (error) {
         res.json({
             status: false,
