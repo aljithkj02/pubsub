ffae0b8b34a6217b464bdf9407b5878017643e22
aljithkj02 <kwdeathlok@gmail.com>
2024-06-19 20:03:49 +0530

    add dummy chats

--- client/src/components/home/ChatBox.tsx
+++ client/src/components/home/ChatBox.tsx
@@ -1,5 +1,43 @@
 import { ChangeEvent, FormEvent, useState } from "react"
 
+const chats = [
+  {
+    id: 1,
+    name: 'Krishna',
+    message: 'How are you buddy',
+    time: '7:30',
+    me: false
+  },
+  {
+    id: 2,
+    name: 'Balram',
+    message: 'I am doing good brother',
+    time: '8:30',
+    me: false
+  },
+  {
+    id: 3,
+    name: 'Ram',
+    message: 'I hope all are doing good',
+    time: '8:31',
+    me: false
+  },
+  {
+    id: 4,
+    name: 'Jithu',
+    message: 'Jai Sree Ram',
+    time: '10:30',
+    me: true
+  },
+  {
+    id: 5,
+    name: 'Sita',
+    message: 'How are you my son Jithu',
+    time: '17:30',
+    me: false
+  },
+];
+
 export const ChatBox = () => {
   const [text, setText] = useState('');
 
@@ -13,21 +51,31 @@ export const ChatBox = () => {
   }
 
   return (
-    <div className="bg-yellow-100 max-h-[91.8vh] h-screen overflow-y-auto relative">
+    <div className="bg-yellow-200 max-h-[91.8vh] h-screen overflow-y-auto relative">
 
       <div className="bg-violet-600 px-8 py-4 text-white font-semibold absolute w-full top-0">
         <p className="text-lg">Room Name</p>
       </div>
 
-      <div className="mt-16 px-5">
-        Chats
+      <div className="mt-20 px-5 flex flex-col gap-2 max-h-[70vh] overflow-y-scroll pb-2">
+        {
+          chats.map((item) => {
+            return (
+              <div key={item.id} className={`bg-white px-4 py-2 max-w-[30%] w-full rounded-lg ${item.me ? 'self-end': 'self-start'}`}>
+                <p className="text-emerald-600 font-semibold">{item.name}</p>
+                <p className="text-gray-500">{item.message}</p>
+                <p className="text-right text-gray-500 text-sm">{item.time}</p>
+              </div>
+            )
+          })
+        }
       </div>
 
       <form onSubmit={handleSendMessage}>
         <div className="absolute bottom-0 bg-emerald-600 w-full px-8 py-3 flex">
           
           <input type="text" value={text} onChange={handleChangeText} autoFocus
-            className="w-full px-2 py-2 rounded-md outline-none rounded-r-none"
+            className="w-full px-6 py-2 rounded-md outline-none rounded-r-none"
           />
 
           <div>
