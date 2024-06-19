cdb0e0d998dba6b0694ebe30dbd69ce45720d0c6
aljithkj02 <kwdeathlok@gmail.com>
2024-06-19 20:19:52 +0530

    add loader

---
+++ client/src/components/shared/Loader.tsx
@@ -0,0 +1,12 @@
+
+export const Loader = () => {
+  return (
+    <div className="fixed top-0 left-0 w-full h-[100vh] bg-black bg-opacity-50 flex justify-center items-center z-30">
+        <div className="w-12 h-12">
+            <img src="https://media.tenor.com/wpSo-8CrXqUAAAAi/loading-loading-forever.gif" 
+                className="w-full h-full object-cover"
+            />
+        </div>
+    </div>
+  )
+}
