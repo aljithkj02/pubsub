0268266442ca47bfac372e60281a591de78b7aea
aljithkj02 <kwdeathlok@gmail.com>
2024-06-20 19:32:21 +0530

    update the roomRouter

--- server/src/routers/room.ts
+++ server/src/routers/room.ts
@@ -1,4 +1,4 @@
-import { addRoom, getAllRooms } from "@controllers/room.controller";
+import { addRoom, getAllMessages, getAllRooms } from "@controllers/room.controller";
 import { authMiddleware } from "@lib/middlewares/auth.middleware";
 import { Router } from "express";
 
@@ -7,3 +7,4 @@ export const roomRouter = Router();
 
 roomRouter.get('/', authMiddleware, getAllRooms);
 roomRouter.post('/', authMiddleware, addRoom);
+roomRouter.get('/:id', authMiddleware, getAllMessages);
